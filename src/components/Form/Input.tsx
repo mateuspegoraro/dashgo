f4b0ae5e11b8e0ebd83d65dfaad0fffeab63195e
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-06-14 08:01:20 -0300

    Componente: Input

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,25 @@
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakraInputProps } from "@chakra-ui/react";
+
+interface InputProps extends ChakraInputProps{
+    name: string;
+    label?: string;
+}
+
+export function Input({name, label, ...rest}: InputProps) {
+    return (
+        <FormControl>
+            {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+            <ChakraInput
+                id={name}
+                name={name}
+                type={name}
+                focusBorderColor="pink.500"
+                bgColor="gray.900"
+                variant="filled"
+                _hover={{bgColor: 'gray.900'}}
+                size="lg"
+                {...rest}
+            />
+        </FormControl>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Flex, Input, Stack, FormLabel, FormControl } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
 export default function Home() {
   return (
@@ -17,33 +18,8 @@ export default function Home() {
           flexDir="column"
           >
             <Stack spacing="4">
-              <FormControl>
-                <FormLabel htmlFor="email">E-mail</FormLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                  focusBorderColor="pink.500"
-                  bgColor="gray.900"
-                  variant="filled"
-                  _hover={{bgColor: 'gray.900'}}
-                  size="lg"
-                  />
-              </FormControl>
-              
-              <FormControl>
-                <FormLabel htmlFor="password">Password</FormLabel>  
-                <Input
-                  id="password"
-                  name="password"
-                  type="password"
-                  focusBorderColor="pink.500"
-                  bgColor="gray.900"
-                  variant="filled"
-                  _hover={{bgColor: 'gray.900'}}
-                  size="lg"
-                  />
-              </FormControl>
+              <Input name="email" label="E-mail" type="email"/>
+              <Input name="password" label="Password" type="password"/>
             </Stack>
             <Button
               type="submit"
