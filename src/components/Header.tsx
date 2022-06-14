360e165616c6e0ad5f443503677b94d10b0b4eca
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-06-14 08:51:34 -0300

    Finalizando Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
     return (
@@ -44,6 +44,35 @@ export function Header() {
                     _placeholder={{ color: "gray.400" }}
                 />
                 <Icon as={RiSearchLine} fontSize="20"/>
+            </Flex>
+
+            <Flex
+                align="center"
+                ml="auto"
+            >
+                <HStack 
+                    spacing="8"
+                    mx="8"
+                    pr="8"
+                    py="1"
+                    color="gray.300"
+                    borderRightWidth={1}
+                    borderColor="gray.700"
+                >
+                    <Icon as={RiNotificationLine} fontSize="20" />
+                    <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+                <Flex 
+                    align="center"
+                >
+                    <Box mr="4" textAlign="right">
+                        <Text>Mateus Pegoraro</Text>
+                        <Text color="gray.300" fontSize="small">
+                            mateus.pegoraro@email.com
+                        </Text>
+                    </Box>
+                    <Avatar size="md" name="Mateus Pegoraro" src="https://github.com/mateuspegoraro.png"/>
+                </Flex>
             </Flex>
         </Flex>
     )
