c37d02885a6ceda387399729867ad9f8713722af
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-06-16 11:44:41 -0300

    Página: Dashboard

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
-import Document, { Html } from "next/document";
-import Head from "next/head";
+import Document, { Html, Main, Head, NextScript } from "next/document";
 
 export default class MyDocument extends Document{
     render() {
@@ -10,6 +9,10 @@ export default class MyDocument extends Document{
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Open+Sans:wght@400;600&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
                 </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
             </Html>
         )
     } 
