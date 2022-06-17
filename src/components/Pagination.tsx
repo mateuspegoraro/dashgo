f404ed05334472ca4a3b591eac41a82453e66f65
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-06-17 12:35:18 -0300

    Componente: Pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,80 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
+
+export function Pagination(){
+    return (
+        <HStack
+            mt="8"
+            justify="space-between"
+            align="center"
+            spacing="6"
+        >
+            <Box>
+                <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+            </Box>
+            <HStack
+                direction="row"
+                spacing="2"
+            >
+                <Button 
+                    size="sm"
+                    fontSize="xs"
+                    width="4"
+                    colorScheme="pink"
+                    disabled
+                    _disabled={
+                        {
+                            bgColor: 'pink.500',
+                            cursor: 'default',
+                        }
+                    }
+                >
+                    1
+                </Button>
+                <Button 
+                    size="sm"
+                    fontSize="xs"
+                    width="4"
+                    bgColor="gray.700"
+                    _hover={{
+                        bg: 'gray.500'
+                    }}
+                >
+                    2
+                </Button>
+                <Button 
+                    size="sm"
+                    fontSize="xs"
+                    width="4"
+                    bgColor="gray.700"
+                    _hover={{
+                        bg: 'gray.500'
+                    }}
+                >
+                    3
+                </Button>
+                <Button 
+                    size="sm"
+                    fontSize="xs"
+                    width="4"
+                    bgColor="gray.700"
+                    _hover={{
+                        bg: 'gray.500'
+                    }}
+                >
+                    4
+                </Button>
+                <Button 
+                    size="sm"
+                    fontSize="xs"
+                    width="4"
+                    bgColor="gray.700"
+                    _hover={{
+                        bg: 'gray.500'
+                    }}
+                >
+                    5
+                </Button>
+            </HStack>
+        </HStack>
+    )
+}
