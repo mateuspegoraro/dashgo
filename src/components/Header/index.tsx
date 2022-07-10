fbdc9ee4cb714c66b2ba151a722f68ba407474c5
Mateus Pegoraro <mateus.pegoraro@involves.com>
2022-07-10 11:53:03 -0300

    Sidebar responsiva

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,17 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
+import { useSidebarDrawer } from '../../context/SidebarDrawerContext'
 import { Logo } from './Logo'
 import { NotificationsNav } from './NotificationsNav'
 import { Profile } from './Profile'
 import { SearchBox } from './SearchBox'
 
 export function Header() {
+    const {onOpen} = useSidebarDrawer()
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
     })
-
     return (
         <Flex
           as="header"
@@ -21,9 +23,17 @@ export function Header() {
           px="6"
           align="center"
         >
+            { !isWideVersion && (
+                <IconButton 
+                    aria-label="Open Navigation"
+                    icon={<Icon as={RiMenuLine}/>}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                />
+            )}
             <Logo />
             {isWideVersion && <SearchBox />}
-
             <Flex
                 align="center"
                 ml="auto"
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import {AppProps} from 'next/app'
-import {ChakraProvider} from '@chakra-ui/react'
-import {theme} from '../styles/theme'
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../styles/theme'
+import { SidebarDrawerProvider } from '../context/SidebarDrawerContext'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme} >
-      <Component {...pageProps} />
+      <SidebarDrawerProvider>
+        <Component {...pageProps} />
+      </SidebarDrawerProvider>
     </ChakraProvider>
   )
 }
